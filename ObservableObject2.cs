1bdf3f56e7013253d316a6555a1090b0baca8bd5
DismissedLight <1686188646@qq.com>
2022-01-25 14:54:23 +0800

    Update ObservableObject2.cs

--- ObservableObject2.cs
+++ ObservableObject2.cs
@@ -16,6 +16,22 @@ namespace Snap.Core.Mvvm
             return result;
         }
 
+        /// <summary>
+        /// 设置属性，且无视属性是否发生变化，触发回调
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="newValue"></param>
+        /// <param name="then"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected bool SetPropertyAndCallbackOverridePropertyState<T>([NotNullIfNotNull("newValue")] ref T field, T newValue, Action then, [CallerMemberName] string? propertyName = null)
+        {
+            bool result = SetProperty(ref field, newValue, propertyName);
+            then.Invoke();
+            return result;
+        }
+
         protected bool SetPropertyAndCallbackOnCompletion<T>([NotNullIfNotNull("newValue")] ref T field, T newValue, Action<T> then, [CallerMemberName] string? propertyName = null)
         {
             bool result = SetProperty(ref field, newValue, propertyName);
